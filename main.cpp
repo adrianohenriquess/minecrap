7d6a07ace15a61ad9271f9af6618f6b8cda6b7ad
adrianohenriquess <adriano.henriquesds@gmail.com>
2023-02-23 19:19:22 -0300

    Adicionando numeros randomicos para printar retangulos em lugares aleatorios da tela

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <SFML/Graphics.hpp>
 //#include <memory>
+//#include <iostream>
+#include <experimental/random>
 
 int main() {
+    std::srand(std::time(nullptr));
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Minecrap - 0.1");
     //ponteiro inteligente
     //auto object = std::make_shared<sf::RectangleShape>();
@@ -9,7 +12,9 @@ int main() {
     sf::RectangleShape object;    
     object.setSize(sf::Vector2f(100.f, 100.f));
     object.setFillColor(sf::Color(0,255,0));
-    object.setPosition(sf::Vector2f(10.f, 10.f));
+
+    float x = static_cast<float>(std::experimental::randint(10, (int)(window.getSize().x - object.getSize().x - 10)));
+    object.setPosition(sf::Vector2f(x, 10.f));
 
     while (window.isOpen()) {
     	sf::Event event;
@@ -18,6 +23,11 @@ int main() {
                   window.close();
         }
 
+        if (object.getPosition().y > window.getSize().y) {
+            //std::cout << "A posição é: " << object.getPosition().y << '\n';
+            x = static_cast<float>(std::experimental::randint(10, (int)(window.getSize().x - object.getSize().x - 10)));
+            object.setPosition(x, 10.f);
+        }
 	    object.move(0.f, 0.9f);
 
         window.clear();
